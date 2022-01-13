8ccfc956858798895b0fba5123d0d4a128cbba00
grazikgafriginal <kaizer.kaizmir@gmail.com>
2022-01-13 22:22:50 +0200

    added polls
    added polls

--- src/commands/general.py
+++ src/commands/general.py
@@ -5,3 +5,10 @@ class GeneralCommands(commands.Cog):
   @commands.command()
   async def hello(self, ctx):
     await ctx.send("Hello!")
+
+  @commands.command()
+  async def poll(self, ctx,*,message):
+    emb=discord.Embed(title=message)
+    ms = await ctx.channel.send(embed=emb)
+    await ms.add_reaction('✅')
+    await ms.add_reaction('❌')
--- src/main.py
+++ src/main.py
@@ -8,5 +8,8 @@ bot = commands.Bot(command_prefix = '.')
 bot.add_cog(AdminCommands())
 bot.add_cog(GeneralCommands())
 
+
+
+
 with open("TOKENDONOTCHECKIN.txt", 'r') as f:
     bot.run(f.read())
